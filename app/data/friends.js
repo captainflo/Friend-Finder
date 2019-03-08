89708cb74f84023f458581430ebc26a98099c45c
florian lahitte <lahitte.florian@gmail.com>
2019-03-08 13:43:21 -0500

    routes, data + html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,56 @@
 // People (DATA)
 // =============================================================
-var people = [
+var friends = [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Margo Robie",
+        "photo":"https://www.biography.com/.image/t_share/MTUxNTgzODk3Mjk2MDUzNTE2/gettyimages-870916736.jpg",
         "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
+            3,
             2,
             5,
             4,
-            1
-          ]
+            5,
+            3,
+            3,
+            3,
+            3,
+            2
+          ]   
     },
-    
+    {
+        "name":"Bradley Cooper",
+        "photo":"https://m.media-amazon.com/images/M/MV5BMjM0OTIyMzY1M15BMl5BanBnXkFtZTgwMTg0OTE0NzE@._V1_.jpg",
+        "scores":[
+            4,
+            5,
+            5,
+            1,
+            2,
+            3,
+            4,
+            1,
+            3,
+            1
+          ]   
+    },
+    {
+        "name":"Chris hemsworth",
+        "photo":"https://vignette.wikia.nocookie.net/disney/images/7/73/Chris_Hemsworth.jpg/revision/latest?cb=20180807225753",
+        "scores":[
+            5,
+            1,
+            2,
+            1,
+            5,
+            1,
+            5,
+            3,
+            3,
+            5
+          ]   
+    },
   ];
+
+  module.exports = friends;
+
+  
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var friends = require("./app/data/friends");
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -11,8 +12,38 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static('./app/public/public'));
 
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./app/public/home.html"));
+  });
+  
+  app.get("/survery", function(req, res) {
+    res.sendFile(path.join(__dirname, "./app/public/survery.html"));
+  });
+  
+  // Displays all people
+  app.get("/api/friends", function(req, res) {
+    return res.json(friends);
+  });
+  
+  // Create New friends - takes in JSON input
+  app.post("/api/friends", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newfriends = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newfriends
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newfriends.id = newfriends.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newfriends);
+  
+    friend.push(newfriends);
+  
+    res.json(newfriends);
+  });
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
